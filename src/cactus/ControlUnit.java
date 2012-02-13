8c11c1d8da72147c0272ad3b598879c8846a150a
Lucas Sa <lucas.sa@gmail.com>
2012-02-13 00:21:12 -0500

    Added basic CPU, CU, and MMU structure

--- src/cactus/ControlUnit.java
+++ src/cactus/ControlUnit.java
@@ -1,6 +1,19 @@
 package cactus;
 
+/**
+ * Control Unit for a CPU
+ */
 public class ControlUnit implements Unit {
- 
+    /**
+     * Parent CPU
+     */
+    private CentralProcessorUnit cpu;
+    
+    /**
+     * Constructor
+     */
+    public ControlUnit(CentralProcessorUnit cpu) {
+        this.cpu = cpu;
+    }
 }
  
