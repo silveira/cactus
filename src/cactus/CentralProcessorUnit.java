5c0849495b13f85133a80b7bbaa68c1909bf9efd
Lucas Sa <lucas.sa@gmail.com>
2012-02-12 23:37:17 -0500

    Added packaging and basic properties of Computer and CPU

--- src/cactus/CentralProcessorUnit.java
+++ src/cactus/CentralProcessorUnit.java
@@ -1,4 +1,48 @@
+package cactus;
+
+/**
+ * Central Processor Unit of a computer, where most of processor activities 
+ * takes place.
+ */
 public class CentralProcessorUnit implements Unit {
- 
+    /* Units */
+    
+    /**
+     * CPU's Control Unit (CU) 
+     */
+    private ControlUnit cu;
+    
+    /**
+     * CPU's Memory Unit (MU)
+     */
+    private MemoryUnit mu;
+    
+    /**
+     * CPU's Arithmetic Logic Unit (ALU)
+     */
+    private ArithmeticLogicUnit alu;
+    
+    /* Register */
+    // TODO: Add GPR's, decide whether vector or several variables
+    
+    /**
+     * Program Counter (PC)
+     */
+    private Register16 pc;
+    
+    /**
+     * Instruction Register (IR)
+     */
+    private Register16 ir;
+    
+    /**
+     * Index Register (X0)
+     */
+    private Register16 x0;    
+    
+    /**
+     * Condition Code Register (CC)
+     */
+    private Register4 cc;
 }
  
--- src/cactus/Computer.java
+++ src/cactus/Computer.java
@@ -1,4 +1,17 @@
+package cactus;
+
+/**
+ * The main class of our computer simulator.
+ * Contains all the necessary parts of the computer.
+ */
 public class Computer {
- 
+    /**
+     * Represents the Central Processor Unit (CPU) of the Computer
+     */
+    private CentralProcessorUnit cpu; 
+    /**
+     * Main memory of the computer, where programs and data are stored.
+     */
+    private Memory memory;
 }
  
