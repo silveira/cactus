acdae14851d0f2058c44e2c861bfa5fc22314864
Ney Mello Sampaio <ney@tsunami.dc.dc.cox.net>
2012-02-14 00:18:04 -0500

    Add Main, Compiler class.

---
+++ src/cactus/utils/Compiler.java
@@ -0,0 +1,16 @@
+package cactus.utils;
+
+/**
+ * 
+ * 
+ */
+public class Compiler {
+    
+    public String compile(String code){
+        //TODO Criar funćão de compilar
+        
+        return "";
+    }
+    
+    
+}
